380331576c2a0e773ffb996a5a87474eb52d0f98
Marcelo Vichar <marcos.marcelo43@gmail.com>
2022-05-23 09:46:25 -0300

    grgerg

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><button class="button start">2</button></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
